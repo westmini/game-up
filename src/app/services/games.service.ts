59da80052bbf0b98e5dbe379c0bb916736d397ee
numan96 <numan1@hotmail.co.uk>
2020-09-02 01:40:18 +0100

    made endpoint dynamic.

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -5,12 +5,21 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
   providedIn: 'root'
 })
 export class GamesService {
-  endPoint = 'http://localhost:4200/genres';
+  endPoint = 'http://localhost:4200';
 
   constructor(private httpClient: HttpClient) {
   }
 
   getGames() {
+    this.endPoint = this.endPoint + '/games';
+    let headers = new HttpHeaders();
+    headers = headers.set('user-key', '3529e735235b970f787422c16376f395');
+    const filters = {fields: '*'};
+    return this.httpClient.post(this.endPoint, filters, {headers});
+  }
+
+  getGenres() {
+    this.endPoint = this.endPoint + '/genres';
     let headers = new HttpHeaders();
     headers = headers.set('user-key', '3529e735235b970f787422c16376f395');
     const filters = {fields: '*'};
